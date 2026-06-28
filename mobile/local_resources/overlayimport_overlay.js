dc16cab24c7c972d468bb70cd0ff655d44053115
The Elder Times <The_Elder_Times@outlook.com>
2026-06-28 14:24:58 +0300

    Change fetch paths to local_resources for overlay

--- mobile/local_resources/overlayimport_overlay.js
+++ mobile/local_resources/overlayimport_overlay.js
@@ -1,9 +1,9 @@
-fetch("resources/overlay/header.html")
+fetch("local_resources/overlay/header.html")
 	.then(response=>response.text())
 	.then(data=>{
 		document.getElementById("import_header").innerHTML=data;
 	})
-fetch("resources/overlay/footer.html")
+fetch("local_resources/overlay/footer.html")
     .then(response=>response.text())
     .then(data=>{
         document.getElementById("import_footer").innerHTML=data;
