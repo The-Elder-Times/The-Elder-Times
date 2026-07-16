b71087a4a49cc4625d70577754fa81c3ae0b08ef
The Elder Times <The_Elder_Times@outlook.com>
2026-07-16 17:02:24 +0300

    Fix localStorage key for documentation page

--- global_resources/documentations.js
+++ global_resources/documentations.js
@@ -1,6 +1,6 @@
 const DEFAULT_PAGE="../global_resources/documentations/index.html";
 const frame=document.getElementById("documentations_frame");
-let page=localStorage.getItem("documentation_page");
+let page=localStorage.getItem("documentations_page");
 if(page===null){
 	page=DEFAULT_PAGE;
 }
