cad4521a4dd66c951e7c90eda63b5cee835f65cc
The Elder Times <The_Elder_Times@outlook.com>
2026-07-17 09:21:19 +0300

    Update default page and iframe ID in documentation

--- global_resources/documentations.js
+++ global_resources/documentations.js
@@ -1,5 +1,5 @@
-const DEFAULT_PAGE="../global_resources/documentations/index.html";
-const frame=document.getElementById("documentations_frame");
+const DEFAULT_PAGE="../global_resources/documentations/main_page.html";
+const frame=document.getElementById("documentations_iframe");
 let page=localStorage.getItem("documentations_page");
 if(page===null){
 	page=DEFAULT_PAGE;
