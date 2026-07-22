733156d83da4a6d2635aa08cf07ebe47069de0fe
The Elder Times <The_Elder_Times@outlook.com>
2026-07-22 09:09:13 +0300

    Remove spaces in event listener callback

--- global_resources/documentations.js
+++ global_resources/documentations.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded",()=>{
 	const DEFAULT_PAGE="../global_resources/documentations/main_page.html";
 	const frame=document.getElementById("documentations_iframe");
 	let page=localStorage.getItem("documentations_page");
