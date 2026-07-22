dc55482511ddbd03a607dee4de8622f52fffddc1
The Elder Times <The_Elder_Times@outlook.com>
2026-07-22 09:08:09 +0300

    Wrap document loading logic in DOMContentLoaded event

--- global_resources/documentations.js
+++ global_resources/documentations.js
@@ -1,7 +1,9 @@
-const DEFAULT_PAGE="../global_resources/documentations/main_page.html";
-const frame=document.getElementById("documentations_iframe");
-let page=localStorage.getItem("documentations_page");
-if(page===null){
-	page=DEFAULT_PAGE;
-}
-frame.src=page;
+window.addEventListener("DOMContentLoaded", () => {
+	const DEFAULT_PAGE="../global_resources/documentations/main_page.html";
+	const frame=document.getElementById("documentations_iframe");
+	let page=localStorage.getItem("documentations_page");
+	if(page===null){
+		page=DEFAULT_PAGE;
+	}
+	frame.src=page;
+});
